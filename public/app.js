e5e3515be8e81d04e646bd13937b09453855bc61
Chris Chalfant <cchalfant@leafsoftwaresolutions.com>
2016-07-04 11:08:49 -0400

    added skip button and view events

--- public/app.js
+++ public/app.js
@@ -41,6 +41,10 @@ learnjs.buildCorrectFlash = function(problemNumber) {
   return correctFlash;
 }
 
+learnjs.triggerEvent = function(name, args) {
+  $('.view-container>*').trigger(name, args);
+}
+
 learnjs.problemView = function(data) {
   var problemNumber = parseInt(data, 10);
   var view = $('.templates .problem-view').clone();
@@ -63,6 +67,15 @@ learnjs.problemView = function(data) {
     return false;
   }
 
+  if (problemNumber < learnjs.problems.length) {
+    var buttonItem = learnjs.template('skip-btn');
+    buttonItem.find('a').attr('href', '#problem-' + (problemNumber + 1));
+    $('.nav-list').append(buttonItem);
+    view.bind('removingView', function() {
+      buttonItem.remove();
+    });
+  }
+
   view.find('.check-btn').click(checkAnswerClick);
   view.find('.title').text('Problem #' + problemNumber);
   learnjs.applyObject(learnjs.problems[problemNumber-1], view);
@@ -82,6 +95,7 @@ learnjs.showView = function(hash) {
   var hashParts = hash.split('-');
   var viewFn = routes[hashParts[0]];
   if (viewFn) {
+    learnjs.triggerEvent('removingView', []);
     $('.view-container').empty().append(viewFn(hashParts[1]));
   }
 }
