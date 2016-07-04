2b547832b136d97179fbbf7f4186acceb6008cb0
Chris Chalfant <cchalfant@leafsoftwaresolutions.com>
2016-07-04 10:35:00 -0400

    flash answer and nav to next problem

--- public/app.js
+++ public/app.js
@@ -25,6 +25,22 @@ learnjs.flashElement = function(elem, content) {
   });
 };
 
+learnjs.template = function(name) {
+  return $('.templates .' + name).clone();
+};
+
+learnjs.buildCorrectFlash = function(problemNumber) {
+  var correctFlash = learnjs.template('correct-flash');
+  var link = correctFlash.find('a');
+  if (problemNumber < learnjs.problems.length) {
+    link.attr('href', '#problem-' + (problemNumber+1));
+  } else {
+    link.attr('href', '');
+    link.text("You're Finished!")
+  }
+  return correctFlash;
+};
+
 learnjs.problemView = function(data) {
   var problemNumber = parseInt(data, 10);
   var view = $('.templates .problem-view').clone();
@@ -39,7 +55,8 @@ learnjs.problemView = function(data) {
 
   function checkAnswerClick() {
     if (checkAnswer()) {
-      learnjs.flashElement(resultFlash, 'Correct!');
+      var correctFlash = learnjs.buildCorrectFlash(problemNumber);
+      learnjs.flashElement(resultFlash, correctFlash);
     } else {
       learnjs.flashElement(resultFlash, 'Incorrect!');
     }
